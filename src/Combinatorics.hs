cbefeb1a23b41b7f88a68dc252f07d2e527e6930
max-min-median <152759160+max-min-median@users.noreply.github.com>
2026-03-24 23:44:51 +0800

    Rewrote 'choose'

--- src/Combinatorics.hs
+++ src/Combinatorics.hs
@@ -2,15 +2,16 @@ module Combinatorics (choose, permute) where
 
 import Numeric.Natural (Natural)
 
+
 choose :: Natural -> Natural -> Natural
 choose n r
   | r > n = 0
   | r == 0 = 1
   | 2 * r > n = n `choose` (n - r)
-  | otherwise = go n r
-  where
-    go n' 1 = n'
-    go n' r' = go (n' - 1) (r' - 1) * n' `div` r'
+  | otherwise = let diff = n - r
+                in foldl' (\acc x -> acc * (x + diff) `div` x) 1 [1..r]
+                -- acc * (x + diff) is always divisible by x at each step
+
 
 permute :: Natural -> Natural -> Natural
 permute n r
